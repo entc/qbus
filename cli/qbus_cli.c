a5bb862932d86d6b618eb900b90298150dd6cef2
Alexander Kalkhof <alexander.kalkhof@dlr.de>
2019-05-28 14:45:24 +0200

    cleanup

--- cli/qbus_cli.c
+++ cli/qbus_cli.c
@@ -1,4 +1,5 @@
 #include "qbus.h"
+#include "sys/cape_log.h"
 
 #include <stdlib.h>
 #include <ncurses.h>
@@ -15,17 +16,45 @@ struct QBusCli_s
 
 static int __STDCALL cli_on_init (QBus qbus, void* ptr, void** p_ptr, CapeErr err)
 {
+  int res;
   QBusCli cli = CAPE_NEW (struct QBusCli_s);
 
-  // set user pointer
-  *p_ptr = cli;
-  
   cli->fd = fopen ("/dev/tty", "r+");
+  
+  if (cli->fd == NULL)
+  {
+    res = cape_err_lastOSError (err);
+    goto exit_and_cleanup;
+  }
 
   cli->scr = newterm (NULL, cli->fd, cli->fd);
-  
-  curs_set(0);
 
+  if (cli->scr == NULL)
+  {
+    res = cape_err_lastOSError (err);
+    goto exit_and_cleanup;
+  }
+
+  set_term (cli->scr);
+
+  cape_log_msg (CAPE_LL_TRACE, "QBUS", "cli", "switched to NCURSES screen");
+  
+  // set user pointer
+  *p_ptr = cli;
+  cli = NULL;
+  
+  
+  mvprintw(0, 0, "hello ncurses");
+  
+  refresh();
+  
+exit_and_cleanup:
+
+  if (cli)
+  {
+    
+  }
+  
   return CAPE_ERR_NONE;
 }
 
@@ -35,12 +64,14 @@ static int __STDCALL cli_on_done (QBus qbus, void* ptr, CapeErr err)
 {
   QBusCli cli = ptr;
   
+  // return the term
+  endwin ();
+
+  // frees storage associated with the SCREEN data structure
   delscreen(cli->scr);
 
   fclose (cli->fd);
-  
-  endwin ();
-  
+    
   CAPE_DEL (&cli, struct QBusCli_s);
   
   return CAPE_ERR_NONE;
