18d25ea648248f45c42e18c2bc473bbd9c227033
Alexander Kalkhof <alex@kalkhof.org>
2019-05-30 04:54:52 +0200

    fix

--- core/qbus_route.c
+++ core/qbus_route.c
@@ -887,6 +887,8 @@ CapeUdc qbus_route_modules (QBusRoute self)
 
 void* qbus_route_add_on_change (QBusRoute self, void* ptr, fct_qbus_on_route_change on_change)
 {
+  CapeListNode n = NULL;
+  
   cape_mutex_lock (self->on_changes_mutex);
 
   {
@@ -895,10 +897,12 @@ void* qbus_route_add_on_change (QBusRoute self, void* ptr, fct_qbus_on_route_cha
     cc->fct = on_change;
     cc->ptr = ptr;
     
-    cape_list_push_back (self->on_changes_callbacks, cc);
+    n = cape_list_push_back (self->on_changes_callbacks, cc);
   }
   
   cape_mutex_unlock (self->on_changes_mutex);
+  
+  return n;
 }
 
 //-----------------------------------------------------------------------------
